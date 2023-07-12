37bf1a986d47ef5f474b0cf276c80663b7a3aeb8
willyguimaraes <willy.barros@ccc.ufcg.edu.br>
2023-07-12 12:47:41 -0300

    adicionando primeiros widgets

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,15 +15,70 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomepage extends StatelessWidget {
-  const MyHomepage({super.key});
+  final _transactions = [
+    Transaction(id: 't1', title: 'tenis', value: 310.76, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'brusinha', value: 211.30, date: DateTime.now())
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Despesas pessoais'),
-      ),
-      body: Center(child: Text('Versão inicial')),
-    );
+        appBar: AppBar(
+          title: Text('Despesas pessoais'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              width: double.infinity,
+              child: Card(
+                child: Text('grafico'),
+                elevation: 5,
+                color: Colors.green,
+              ),
+            ),
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                  child: Row(children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Color.fromARGB(255, 112, 22, 128),
+                        width: 2,
+                      )),
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(tr.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                      )),
+                       Text(tr.date.toString(),
+                       style: TextStyle( color: Colors.grey))],
+                    )
+                  ]),
+                );
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
